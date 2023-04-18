7a2d5bc02523ba6fe307d7fccdd15ecb98e4c88f
Aman Mittal <amanmittalma@gmail.com>
2023-04-18 22:28:46 +0530

    AND OR Logical Operators

--- React/Learning_JSX/Conditional_Rendering.js
+++ React/Learning_JSX/Conditional_Rendering.js
@@ -1,21 +1,35 @@
-const User = (name) =>{
-    let logIn = true;
-    return(
-        <>
-        <h1>Hello {name}!</h1>
-        <h2>Hello {logIn?"Alexa":"User"}</h2>
-        </>
-    );
-}
+const User = (name) => {
+  let logIn = true;
+  let fname = "Virat";
+  let lname = "";
+  return (
+    <>
+      <h1>Hello {name}!</h1>
+      <h2>{name == "Aman" ? "Welcome to the portal" : ""}</h2>
+      <h2>Hello {logIn ? "Alexa" : "User"}</h2>
+      <h2>{logIn && true ? "Welcome to the Portal" : ""}</h2>
+      {logIn && <h2>Welcome Aman</h2>}
+      {/* <div>
+        {fname && lname ? (
+          <h2>
+            {lname} {fname}
+          </h2>
+        ) : (
+          <h2>{fname}</h2>
+        )}
+      </div> */}
+      <h2>Hello {logIn?lname || fname:fname}!</h2>
+    </>
+  );
+};
 
-function App(){
-    let loggedIn = false;
-    if(loggedIn){
-        return User("Aman");
-    }
-    else{
-        return User('User!');
-    }
+function App() {
+  let loggedIn = true;
+  if (loggedIn) {
+    return User("Aman");
+  } else {
+    return User("User!");
+  }
 }
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
