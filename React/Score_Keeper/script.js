0dfc73808bdddd4db995bc1f468506e756f0750f
Aman Mittal <amanmittalma@gmail.com>
2023-04-23 15:24:46 +0530

    Score Keeper App

--- React/Score_Keeper/script.js
+++ React/Score_Keeper/script.js
@@ -1,6 +1,8 @@
 var score = 0;
 var wicket = 0;
 let ballWiseRes = [];
+let hit;
+let textInput = React.createRef();
 
 // function clickOne(val){
 //     score = score + val;
@@ -21,59 +23,85 @@ let ballWiseRes = [];
 // }
 
 function addScore(val) {
-  if (wicket < 10) {
-    ballWiseRes.push(val);
-    score += val;
-    rootElement.render(<App />);
-    console.log(ballWiseRes);
-  }
+  // if (wicket < 10) {
+  //   ballWiseRes.push(val);
+  //   score += val;
+  //   rootElement.render(<App />);
+  //   console.log(ballWiseRes);
+  // }
+  hit = val;
+  rootElement.render(<App />);
 }
 
 function addWicket(val) {
-  if (wicket < 10) {
-    ballWiseRes.push("W");
-    wicket += val;
-    rootElement.render(<App />);
-    console.log(ballWiseRes);
-  }
+  // if (wicket < 10) {
+  //   ballWiseRes.push("W");
+  //   wicket += val;
+  //   rootElement.render(<App />);
+  //   console.log(ballWiseRes);
+  // }
+  hit = "W";
+  rootElement.render(<App />);
 }
 
-const ScoreButtons = () =>(
+const ScoreButtons = () => (
   <div>
-      <button onClick={() => addScore(0)}>0</button>
-      <button onClick={() => addScore(1)}>1</button>
-      <button onClick={() => addScore(2)}>2</button>
-      <button onClick={() => addScore(3)}>3</button>
-      <button onClick={() => addScore(4)}>4</button>
-      <button onClick={() => addScore(5)}>5</button>
-      <button onClick={() => addScore(6)}>6</button>
-      <button onClick={() => addWicket(1)} className = "wicket">wicket</button>
-    </div>
-)
+    <button onClick={() => addScore(0)}>0</button>
+    <button onClick={() => addScore(1)}>1</button>
+    <button onClick={() => addScore(2)}>2</button>
+    <button onClick={() => addScore(3)}>3</button>
+    <button onClick={() => addScore(4)}>4</button>
+    <button onClick={() => addScore(5)}>5</button>
+    <button onClick={() => addScore(6)}>6</button>
+    <button onClick={() => addWicket(1)} className="wicket">
+      wicket
+    </button>
+  </div>
+);
 /* {(ind+1) % 6 == 0? <p>{(ind+1)/6} Over</p>:null} */
 
-const Result = () =>(
+const Result = () => (
   <div>
-    {ballWiseRes.map((ele,ind) =>(
+    {ballWiseRes.map((ele, ind) => (
       <>
-      {ind % 6 == 0? <br />:null}
-      <span key = {ind}  style ={{color : ele === "W"?"red":null}}>{ele === 0?<strong>.</strong>:ele} &nbsp; &nbsp;</span>
+        {ind % 6 == 0 ? <br /> : null}
+        <span key={ind} style={{ color: ele === "W" ? "red" : null }}>
+          {ele === 0 ? <strong>.</strong> : ele} &nbsp; &nbsp;
+        </span>
       </>
     ))}
   </div>
-)
+);
 
-function handleSubmit(event){
+function handleSubmit(event) {
   event.preventDefault();
+  const type = event.type;
+  console.log(type);
+  ballWiseRes.unshift(
+    // <span>
+    //   {hit},{textInput.current.value}
+    // </span>
+      <span>{`${hit}, ${textInput.current.value}`}</span>
+  );
+  hit  = 0;
+  textInput.current.value = "";
+  if (hit === "W" && wicket < 10) {
+    wicket += 1;
+  } else if (wicket < 10) {
+    score += hit;
+  }
+  // text = event.target.value;
+  // console.log(textInput.current);
+  rootElement.render(<App />);
 }
 
-const Form = () =>(
-  <form onSubmit = {handleSubmit}>
-    <input type = "text" />
-    <input type = "text"/>
+const Form = () => (
+  <form onSubmit={handleSubmit}>
+    <input type="text" value={hit} />
+    <input type="text" placeholder="Add Comment" ref={textInput} />
     <button>Submit</button>
   </form>
-)
+);
 
 const App = () => (
   <>
@@ -82,10 +110,13 @@ const App = () => (
       SCORE: {score}/{wicket}
     </h2>
     <ScoreButtons />
-    <br/>
+    <br />
     <Form />
     <hr />
-    
+
+    {ballWiseRes.map((res, ind) => (
+      <p key={ind}>{res}</p>
+    ))}
   </>
 );
 
