b509e761034bb16e0f56974e5f9e3deb67dc56a5
Aman Mittal <amanmittalma@gmail.com>
2023-04-29 23:18:55 +0530

    React Components 1

--- React/React_Components_1/movie_app/src/MovieCard.js
+++ React/React_Components_1/movie_app/src/MovieCard.js
@@ -5,7 +5,98 @@ import plus from "./plus.png";
 import minus from "./minus-sign.png";
 
 class MovieCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Title: "The Avengers",
+      plot: "Super Natural powers shown in the movie",
+      price: 199,
+      rating: 8.9,
+      stars: 0,
+    };
+    // this.addStars = this.addStars.bind(this);
+    let a;
+  }
+
+  addStars = () => {
+    if (this.state.stars >= 5) {
+      return;
+    }
+    // console.log("Stars Are Added");
+    // console.log("this :",this.state)
+    // first form
+
+    // this.setState({
+    //   stars:this.state.stars +1
+    // }, () => {
+    //   this.setState({
+    //     stars:this.state.stars +1
+    //   })
+    // })
+    // this.setState({
+    //   stars:this.state.stars +0.5
+    // })
+    // this.setState({
+    //   stars:this.state.stars +0.5
+    // })
+    // this.setState({
+    //   stars:this.state.stars +0.5
+    // })
+
+    //Second form
+    // this.setState(
+    //   (prevState) => {
+    //     return {
+    //       stars: prevState.stars + 0.5,
+    //     };
+    //   },
+    //   () => console.log("stars :", this.state.stars)
+    // );
+    // this.setState(
+    //   (prevState) => {
+    //     return {
+    //       stars: prevState.stars + 0.5,
+    //     };
+    //   },
+    //   () => console.log("stars :", this.state.stars)
+    // );
+    // this.setState(
+    //   (prevState) => {
+    //     return {
+    //       stars: prevState.stars + 0.5,
+    //     };
+    //   },
+    //   () => console.log("stars :", this.state.stars)
+    // );
+    // this.setState(
+    //   (prevState) => {
+    //     return {
+    //       stars: prevState.stars + 0.5,
+    //     };
+    //   },
+    //   () => console.log("stars :", this.state.stars)
+    // );
+
+    console.log("this :", this.state.stars);
+  };
+
+  decStars = () => {
+    if (this.state.stars <= 0) {
+      return;
+    }
+
+    this.setState({
+      stars: this.state.stars - 0.5,
+    });
+
+    // this.setState((prevState) =>{
+    //   return{
+    //     stars:prevState.stars-0.5
+    //   }
+    // })
+  };
   render() {
+    const { Title, plot, price, rating, stars } = this.state;
     return (
       <div className="main">
         <div className="movie-card">
@@ -13,16 +104,26 @@ class MovieCard extends Component {
             <img alt="Poster" src={title} />
           </div>
           <div className="right">
-            <div className="title">The Avengers</div>
-            <div className="plot">Super Natural powers shown in the movie</div>
-            <div className="price">Rs. 199</div>
+            <div className="title">{Title}</div>
+            <div className="plot">{plot}</div>
+            <div className="price">{price}</div>
             <div className="footer">
-              <div className="rating">8.9</div>
+              <div className="rating">{rating}</div>
               <div className="star-dis">
-                <img className="str-btn" alt="increment" src={minus} />
+                <img
+                  className="str-btn"
+                  alt="decrement"
+                  src={minus}
+                  onClick={this.decStars}
+                />
                 <img className="stars" src={star} alt="star" />
-                <img className="str-btn" alt="increment" src={plus} />
-                <span>0</span>
+                <img
+                  className="str-btn"
+                  alt="increment"
+                  src={plus}
+                  onClick={this.addStars}
+                />
+                <span>{stars}</span>
               </div>
               <button className="favourite-btn">Favourite</button>
               <button className="cart-btn">Add to Cart</button>
