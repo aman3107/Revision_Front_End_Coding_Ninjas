9de886d6c2c6f5cb0d22e7b7c9d3879552b596b1
Aman Mittal <amanmittalma@gmail.com>
2023-04-29 15:32:49 +0530

    User Profile

---
+++ assignment_codes_react/src/About.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class About extends Component{
+    render(){
+        return(
+            <div className="About">
+                <p>Hi, my name is Pranav. I am a full stack web developer and I have developed several projects with MERN stack. I am also familiar with Python and Django.</p>
+            </div>
+        )
+    }
+}
