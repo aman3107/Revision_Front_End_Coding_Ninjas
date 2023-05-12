58037dc0a48f4f9317f971e587d73008461c3e48
Aman Mittal <amanmittalma@gmail.com>
2023-05-12 11:00:22 +0530

    React

--- assignment_codes_react/src/App.js
+++ assignment_codes_react/src/App.js
@@ -1,15 +1,46 @@
+import React from "react";
 import "./styles.css";
-import Hero from "./Hero"
-import Skills from "./Skills"
-import About from "./About"
 
-export default function App() {
-  // Code here
-  return(
-    <div className="App">
-      <Hero />
-      <Skills />
-      <About />
-    </div>
-  )
+const MOODS = [
+  "happy",
+  "sad",
+  "angry",
+  "excited",
+  "fearful",
+  "disgusted",
+  "surprised",
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prediction: "",
+    };
+  }
+
+  handleClick() {
+    const moodIndex = Math.floor(Math.random() * MOODS.length);
+    this.setState({ 
+      prediction: MOODS[moodIndex] 
+    });
+  }
+
+  render() {
+    const {currentPrediction} = this.state.prediction;
+    return (
+      <div>
+        <h1>Mood Predictor</h1>
+        {{currentPrediction} && (
+          <p>
+            Mood predictor has predicted that you mood will be
+            <b> {currentPrediction} </b>today.
+          </p>
+        )}
+        <button onClick={this.handleClick}>Predict Mood</button>
+      </div>
+    );
+  }
 }
+
+export default App;
