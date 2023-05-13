7c55a12b2412f8701d4075643310c52d3c6a23c1
Aman Mittal <amanmittalma@gmail.com>
2023-05-13 15:02:17 +0530

    Staring JavaScript Again

---
+++ JAVASCRIPT/Starting_With_JavaScript/script.js
@@ -0,0 +1,49 @@
+// console.log("Hello World");
+// window.alert("Hey");
+
+/* Variables */
+
+// var a = 10;
+// console.log(a);
+// a = "hello";
+// console.log(a);
+// var b = 10.6;
+// console.log(b);
+
+/* Swap the Value */
+
+// var x = 10;
+// var y = 20;
+// var temp = x;
+// x = y;
+// y = temp;
+// console.log(x , y);
+
+/* Null vs Undefined */
+
+// var address;
+// console.log(address);
+// address = null;
+// console.log(address);
+
+/* Type Of */
+
+// var a = 1/0;
+// console.log(a);
+// console.log(a + 1);
+// console.log(a * 1);
+// console.log(1 / a);
+// console.log(Math.pow(10,309));
+// console.log(Number.MAX_VALUE);
+// console.log(Math.sqrt(-1));
+// console.log(parseInt("aaa"));
+// console.log(typeof a);
+// a = 's';
+// console.log(typeof a);
+// a = true;
+// console.log(typeof a);
+// a = null;
+// console.log(typeof a);
+
+/* Operators */
+
