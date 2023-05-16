54f21d1f0dad7f17cb7dc0e129be84d9685d32b6
Aman Mittal <amanmittalma@gmail.com>
2023-05-16 13:51:59 +0530

    Starting of JavaScript

--- JAVASCRIPT/Starting_With_JavaScript/script.js
+++ JAVASCRIPT/Starting_With_JavaScript/script.js
@@ -47,3 +47,79 @@
 
 /* Operators */
 
+// console.log(10+2);
+// console.log(10-2);
+// console.log(13/2);
+// console.log(12.5 % 3);
+// var a = 10;
+// a += 20;
+// console.log(a);
+// a++;
+// console.log(a++);
+// ++a;
+// console.log(a);
+
+// var x = 26;
+// var y = 100;
+
+// console.log(x < 20 || y++ > 99);
+// console.log(y);
+
+
+/* Type Coercion */
+
+// console.log("Hello" + 'World');
+// console.log("Hello"+2);
+// console.log("1"+1)
+// console.log('1' - 1);
+
+/* Conditionals */
+
+// var a = -1;
+// if(a > 0){
+//     console.log("Positive");
+// }
+// else if(a < 0){
+//     console.log("Negative");
+// }
+// else{
+//     console.log("zero");
+// }
+
+/* == vs === */
+
+// console.log(1 == '1');
+// console.log(1 === '1');
+// console.log(1 != '1');
+// console.log(1 !== '1');
+
+/* Loops */
+
+// var i = 1;
+// while(i <= 5){
+//     console.log(i);
+//     i++;
+// }
+
+// for(var i = 1; i <= 5; i++){
+//     console.log(i);
+// }
+
+/* Calculate BMI */
+
+// var weight = 58;
+// var height = 1.6;
+// var bmi = weight/(height*height);
+// console.log("Your Body Mass Index is: "+bmi+" and ");
+// if(bmi < 18.5){
+//     console.log("You are Underweight");
+// }
+// else if(bmi > 18.5 && bmi < 25.0){
+//     console.log("You are Healthy");
+// }
+// else if(bmi > 25 && bmi < 26.0){
+//     console.log("You are Overweight");
+// }
+// else{
+//     console.log("You are Obese")
+// }
