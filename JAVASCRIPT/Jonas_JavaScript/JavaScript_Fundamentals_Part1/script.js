35a8d0402d113dbc3cce1de858fabace02cf30a0
Aman Mittal <amittal@mirrahealthcare.com>
2024-05-21 14:26:14 +0530

    html

---
+++ JAVASCRIPT/Jonas_JavaScript/JavaScript_Fundamentals_Part1/script.js
@@ -0,0 +1,15 @@
+// var hello = document.getElementById("Hello");
+// hello.style.backgroundColor = "red";
+// alert('Hello World');
+let js = "amazing";
+js = "boring";
+if (js === "amazing") {
+  alert("JavaScript is fun");
+}
+
+// let a = "See";
+// let b = "You";
+// let c = "Tomorrow";
+// let d = a + " " + b + " " + c;
+// let str = 'a = See \nb = You \nc = Tomorrow';
+// console.log(str + '\n' + d);
